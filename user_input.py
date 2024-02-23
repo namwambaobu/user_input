9c8845f646b5c72833b35d478e390dea32867440
namwambaobu <157889058+namwambaobu@users.noreply.github.com>
2024-02-23 12:17:02 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,19 @@
+# user_input.py
+
+# Ask the user for their name
+name = input("Please enter your name: ")
+
+# Ask the user for their age
+age = input("Please enter your age: ")
+
+# Ask the user for their location
+location = input("Please enter your location: ")
+
+#just a btw hobby
+hobby = input("Please enter your Hobby: ")
+
+# Print out a personalized message
+print(f"Hello {name}")
+print(f"You are {age} years old")
+print(f"You live in {location}")
+print(f"Your hobby is {hobby}")
